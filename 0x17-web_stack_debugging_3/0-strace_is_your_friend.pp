574d5cb23c8c3d864d913706479b70ac00d4c432
Opeyemi Odebode <odebodeopeyemi@gmail.com>
2022-11-03 04:32:52 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# Fixes why Apache is returning a 500 error
 
-exec { 'fix-wordpress':
+exec { 'fix-apache':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
